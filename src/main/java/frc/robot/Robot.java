4da88c533f10d3b54141c3092692b6958198165a
Eric Rao <ericrao08@gmail.com>
2025-02-19 11:56:49 -0500

    updated pid control for neo motor on tank

--- src/main/java/frc/robot/Robot.java
+++ src/main/java/frc/robot/Robot.java
@@ -4,13 +4,13 @@
 
 package frc.robot;
 
-import com.ctre.phoenix.motorcontrol.ControlMode;
-import com.ctre.phoenix.motorcontrol.can.TalonSRX;
-import edu.wpi.first.wpilibj.Joystick;
-import edu.wpi.first.wpilibj.TimedRobot;
-import edu.wpi.first.wpilibj.Timer;
-import edu.wpi.first.wpilibj.motorcontrol.Spark;
-import edu.wpi.first.wpilibj.motorcontrol.VictorSP;
+import com.ctre.phoenix.motorcontrol.can.*; //libraries, simplified
+import com.revrobotics.spark.*;
+import com.revrobotics.spark.SparkLowLevel.MotorType;
+import edu.wpi.first.wpilibj.*;
+import edu.wpi.first.wpilibj.CounterBase.EncodingType;
+
+import edu.wpi.first.wpilibj.smartdashboard.SmartDashboard; //dashboard to read values
 
 /**
  * The methods in this class are called automatically corresponding to each mode, as described in
@@ -23,41 +23,104 @@ public class Robot extends TimedRobot {
    * initialization code.
    */
 
-  private TalonSRX LeftMasterMotor1 = new TalonSRX(18);
+  private TalonSRX LeftMasterMotor1 = new TalonSRX(18); // 4 drive motors
   private TalonSRX LeftMasterMotor2 = new TalonSRX(3);
   private TalonSRX RightMasterMotor1 = new TalonSRX(4);
   private TalonSRX RightMasterMotor2 = new TalonSRX(1);
 
+  private SparkMax PIDMotor = new SparkMax(13, MotorType.kBrushless); //test neo motor
 
-  private Joystick joy1 = new Joystick(0);
+  private Joystick joy1 = new Joystick(0); //joystick
+
+  private final double kDriveTick2Feet = 1.0 / 128 * 6 * Math.PI / 12;
 
   public Robot() {}
 
   @Override
-  public void robotPeriodic() {}
+  public void autonomousInit() {
+  }
 
   @Override
-  public void autonomousInit() {}
+  public void autonomousPeriodic() {
+  }
+   
+  final double kP = 0.3; //kP value for PID
+  final double kI = 0.5;
+  final double kD = 0.1;
+  final double iLimit = 1;
+  final double maxSpeed = 0.5;
+
+  double setpoint = 0; //target encoder value
+  double errorSum = 0;
+  double lastTimestamp = Timer.getFPGATimestamp();
+  double lastError = 0;
 
   @Override
-  public void autonomousPeriodic() {}
+  public void robotPeriodic() {
+  }
 
   @Override
-  public void teleopInit() {}
+  public void teleopInit() {
+    SmartDashboard.putString("Auto on?", "yes");
+    PIDMotor.getEncoder().setPosition(0); //reset encoder
+    errorSum = 0;
+    lastError = 0;
+    lastTimestamp = Timer.getFPGATimestamp();
+  }
 
   @Override
   public void teleopPeriodic() {
+    /*
     double speed = -joy1.getRawAxis(1) * 0.6;
     double turn = joy1.getRawAxis(4) * 0.3;
 
     double left = speed + turn;
     double right = speed - turn;
 
+    
     LeftMasterMotor1.set(ControlMode.PercentOutput, left);
     LeftMasterMotor2.set(ControlMode.PercentOutput, left);
     RightMasterMotor1.set(ControlMode.PercentOutput, -right);
     RightMasterMotor2.set(ControlMode.PercentOutput, -right);
+    */
+
+    if (joy1.getRawButton(1)){ //go to position 10
+      setpoint = 10;
+    } else if (joy1.getRawButton(2)){
+      setpoint = 0;
+    }
+
+    //calculating speed with PID
+    double sensorPosition = PIDMotor.getEncoder().getPosition() * kDriveTick2Feet;  
+    double error = setpoint - sensorPosition;
+    double dt = Timer.getFPGATimestamp() - lastTimestamp; 
+    if (Math.abs(error) < iLimit){
+      errorSum += error * dt;
+    }
+
+    double errorRate = (error  - lastError) / dt;
+
+    double outputSpeed = kP * error; //+ kI * errorSum + kD * errorRate;
+
+    //caps the output speed between -maxSpeed and maxSpeed
+    outputSpeed = Math.max(-maxSpeed, Math.min(maxSpeed, outputSpeed));
+
+    PIDMotor.set(outputSpeed);
+
+    lastTimestamp = Timer.getFPGATimestamp(); 
+    lastError = error;
+
+    //outputting values
+    //SmartDashboard.putNumber("encoder value", PIDMotor.getEncoder().getPosition() * kDriveTick2Feet); //output to dashboard
+    SmartDashboard.putNumber("speed", outputSpeed);
+    SmartDashboard.putNumber("kP", kP*error);
+    /*
+    SmartDashboard.putNumber("kI", kI*errorSum);
+    SmartDashboard.putNumber("kD", kD*errorRate);
+    */
   }
+
+  /*
   @Override
   public void disabledInit() {}
 
@@ -75,4 +138,5 @@ public class Robot extends TimedRobot {
 
   @Override
   public void simulationPeriodic() {}
+  */
 }
