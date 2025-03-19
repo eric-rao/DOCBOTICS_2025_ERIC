c39fee78a5448cf30a3715ed2e04776c731a17a1
Eric Rao <ericrao08@gmail.com>
2025-03-19 17:35:15 -0400

    updated pid to work for driving as well as extension motor

--- src/main/java/frc/robot/Robot.java
+++ src/main/java/frc/robot/Robot.java
@@ -4,65 +4,73 @@
 
 package frc.robot;
 
-import com.ctre.phoenix.motorcontrol.can.*; //libraries, simplified
-import com.revrobotics.spark.*;
+import com.ctre.phoenix.motorcontrol.ControlMode;
+import com.ctre.phoenix.motorcontrol.can.TalonSRX;
+import com.revrobotics.spark.SparkMax;
 import com.revrobotics.spark.SparkLowLevel.MotorType;
-import edu.wpi.first.wpilibj.*;
-import edu.wpi.first.wpilibj.CounterBase.EncodingType;
+import edu.wpi.first.wpilibj.smartdashboard.SmartDashboard;
 
-import edu.wpi.first.wpilibj.smartdashboard.SmartDashboard; //dashboard to read values
+import edu.wpi.first.wpilibj.Joystick;
+import edu.wpi.first.wpilibj.TimedRobot;
+import edu.wpi.first.wpilibj.Timer;
 
 /**
  * The methods in this class are called automatically corresponding to each mode, as described in
- * the TimedRobot documentation. If you change the name of this class or the package after creating
- * this project, you must also update the Main.java file in the project.
+ * the TimedRobot documentation.
  */
 public class Robot extends TimedRobot {
-  /**
-   * This function is run when the robot is first started up and should be used for any
-   * initialization code.
-   */
 
-  private TalonSRX LeftMasterMotor1 = new TalonSRX(18); // 4 drive motors
+  // Drive motors
+  private TalonSRX LeftMasterMotor1 = new TalonSRX(18);
   private TalonSRX LeftMasterMotor2 = new TalonSRX(3);
   private TalonSRX RightMasterMotor1 = new TalonSRX(4);
   private TalonSRX RightMasterMotor2 = new TalonSRX(1);
 
-  private SparkMax PIDMotor = new SparkMax(13, MotorType.kBrushless); //test neo motor
+  // PID-controlled NEO motor
+  private SparkMax PIDMotor = new SparkMax(13, MotorType.kBrushless); 
 
-  private Joystick joy1 = new Joystick(0); //joystick
+  // Joystick
+  private Joystick joy1 = new Joystick(0); 
 
-  private final double kDriveTick2Feet = 1.0 / 128 * 6 * Math.PI / 12;
+  // Encoder conversion factor (ticks to feet)
+  private final double kDriveTick2Feet = 1.0 / (128 * 6 * Math.PI / 12);
 
-  public Robot() {}
-
-  @Override
-  public void autonomousInit() {
-  }
-
-  @Override
-  public void autonomousPeriodic() {
-  }
-   
-  final double kP = 0.3; //kP value for PID
+  // PID Constants
+  final double kP = 0.3;
   final double kI = 0.5;
   final double kD = 0.1;
   final double iLimit = 1;
   final double maxSpeed = 0.5;
 
-  double setpoint = 0; //target encoder value
-  double errorSum = 0;
-  double lastTimestamp = Timer.getFPGATimestamp();
-  double lastError = 0;
+  // PID Variables
+  double setpoint = 0;       // Target position
+  double errorSum = 0;        // Sum of errors for integral term
+  double lastTimestamp = 0;   // Time tracking for delta time
+  double lastError = 0;       // Previous error for derivative term
+
+  public Robot() {}
 
   @Override
-  public void robotPeriodic() {
+  public void robotInit() {
+    SmartDashboard.putString("Status", "Robot initialized.");
+  }
+
+  @Override
+  public void autonomousInit() {
+    SmartDashboard.putString("Mode", "Autonomous");
+  }
+
+  @Override
+  public void autonomousPeriodic() {
+    // Autonomous code here
   }
 
   @Override
   public void teleopInit() {
-    SmartDashboard.putString("Auto on?", "yes");
-    PIDMotor.getEncoder().setPosition(0); //reset encoder
+    SmartDashboard.putString("Mode", "Teleop");
+    
+    // Reset encoder and PID values
+    PIDMotor.getEncoder().setPosition(0.0);  
     errorSum = 0;
     lastError = 0;
     lastTimestamp = Timer.getFPGATimestamp();
@@ -70,54 +78,61 @@ public class Robot extends TimedRobot {
 
   @Override
   public void teleopPeriodic() {
-    /*
-    double speed = -joy1.getRawAxis(1) * 0.6;
-    double turn = joy1.getRawAxis(4) * 0.3;
+    // Setpoints for PID control based on joystick buttons
+    if (joy1.getRawButton(1)) {    
+      setpoint = 10;               // Go to position 10
+    } else if (joy1.getRawButton(2)) {
+      setpoint = 0;                // Return to position 0
+    }
+
+    // Manual driving
+    double speed = -joy1.getRawAxis(1) * 0.6;  // Forward/backward
+    double turn = joy1.getRawAxis(4) * 0.3;    // Turning
 
     double left = speed + turn;
     double right = speed - turn;
 
-    
-    LeftMasterMotor1.set(ControlMode.PercentOutput, left);
-    LeftMasterMotor2.set(ControlMode.PercentOutput, left);
-    RightMasterMotor1.set(ControlMode.PercentOutput, -right);
-    RightMasterMotor2.set(ControlMode.PercentOutput, -right);
-    */
-
-    if (joy1.getRawButton(1)){ //go to position 10
-      setpoint = 10;
-    } else if (joy1.getRawButton(2)){
-      setpoint = 0;
-    }
-
-    //calculating speed with PID
+    // PID calculations
     double sensorPosition = PIDMotor.getEncoder().getPosition() * kDriveTick2Feet;  
     double error = setpoint - sensorPosition;
-    double dt = Timer.getFPGATimestamp() - lastTimestamp; 
-    if (Math.abs(error) < iLimit){
+    double dt = Timer.getFPGATimestamp() - lastTimestamp;
+
+    // Integral accumulation
+    if (Math.abs(error) < iLimit) {
       errorSum += error * dt;
     }
 
-    double errorRate = (error  - lastError) / dt;
+    // Derivative calculation
+    double errorRate = (error - lastError) / dt;
 
-    double outputSpeed = kP * error; //+ kI * errorSum + kD * errorRate;
+    // PID output calculation
+    double outputSpeed = kP * error + kI * errorSum + kD * errorRate;
 
-    //caps the output speed between -maxSpeed and maxSpeed
+    // Clamp output speed
     outputSpeed = Math.max(-maxSpeed, Math.min(maxSpeed, outputSpeed));
 
+    // Set PID-controlled motor
     PIDMotor.set(outputSpeed);
 
-    lastTimestamp = Timer.getFPGATimestamp(); 
+    // Drive motors manually if PID is inactive
+    if (!joy1.getRawButton(1) && !joy1.getRawButton(2)) {
+      LeftMasterMotor1.set(ControlMode.PercentOutput, left);
+      LeftMasterMotor2.set(ControlMode.PercentOutput, left);
+      RightMasterMotor1.set(ControlMode.PercentOutput, -right);
+      RightMasterMotor2.set(ControlMode.PercentOutput, -right);
+    }
+
+    // Update timestamps and errors
+    lastTimestamp = Timer.getFPGATimestamp();
     lastError = error;
 
-    //outputting values
-    //SmartDashboard.putNumber("encoder value", PIDMotor.getEncoder().getPosition() * kDriveTick2Feet); //output to dashboard
-    SmartDashboard.putNumber("speed", outputSpeed);
-    SmartDashboard.putNumber("kP", kP*error);
-    /*
-    SmartDashboard.putNumber("kI", kI*errorSum);
-    SmartDashboard.putNumber("kD", kD*errorRate);
-    */
+    // Output values to SmartDashboard
+    SmartDashboard.putNumber("Encoder Value (Feet)", sensorPosition);
+    SmartDashboard.putNumber("Setpoint", setpoint);
+    SmartDashboard.putNumber("Output Speed", outputSpeed);
+    SmartDashboard.putNumber("kP Term", kP * error);
+    SmartDashboard.putNumber("kI Term", kI * errorSum);
+    SmartDashboard.putNumber("kD Term", kD * errorRate);
   }
 
   /*
